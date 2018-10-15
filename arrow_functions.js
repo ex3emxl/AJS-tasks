4697c77423b5b22faf60a97592c0ece71b20a15f
Sergii <sergii@mabbly.com>
2018-10-15 20:15:21 +0300

    feat(functions): add new transformed functions

--- arrow_functions.js
+++ arrow_functions.js
@@ -1,8 +1,8 @@
 //Переделать функцию с использованием функции-стрелки 
 //(в методе reduce тоже использовать arrow function):
 
-function sum(...params) {  
-  return (!params.length) ? 0 : params.reduce( (prev, next) => prev + next );
+function sum(...params) {
+    return (!params.length) ? 0 : params.reduce((prev, next) => prev + next);
 }
 
 
@@ -10,10 +10,29 @@ function sum(...params) {
 // значение поля prefix:
 
 const utils = {
-    numbers: [1,2,3,4],
+    numbers: [1, 2, 3, 4],
     prefix: 'number',
     getOdd: function () {
         return this.numbers.map(number => `${this.prefix} - ${number}`)
     }
 };
 
+//Переделать функции в стрелочные функции, использовать,
+//где возможно, короткий синтаксис:
+
+function getDate() { return new Date(); }
+
+getDate = () => new Date();
+
+
+function getDay() {
+  const days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+  return days[new Date().getDay()];
+}
+
+getDay = (days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб']) => days[new Date().getDay()];
+
+
+function getListCopy(list) { return list.slice(); }
+
+getListCopy = (list) => list.slice();
