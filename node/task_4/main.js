f626eb38b44af56c11ef12911da6ec34fe721957
Sergii <sergii@mabbly.com>
2018-11-10 01:42:20 +0200

    feat(node): task_4

---
+++ node/task_4/main.js
@@ -0,0 +1,21 @@
+var fs = require('file-system');
+const currentDate = require('./date');
+const dir_name = 'date_dir';
+const file_name = 'current_date';
+const promise = new Promise((resolve, reject) => {
+        fs.mkdir(dir_name, (err) => {
+            if (err)
+                return reject(err);
+            resolve(dir_name);
+        });
+    });
+
+promise.then( 
+  (data) => new Promise((resolve, reject) => {
+        fs.writeFile(data + '/' + file_name + '.txt', currentDate(), (err) => {
+            if (err)
+                return reject(err);
+            resolve(console.log('File ' + file_name +  ' created!'));
+        });
+    })
+);
