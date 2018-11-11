c0256b4c1bebd4ce999310fc2f65951c2da58958
Sergii <sergii@mabbly.com>
2018-11-11 14:54:42 +0200

    feat(server task_1): created template and server which sends any images

---
+++ node-server/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const isHTML = req.url.includes('.html');
+    const fileName = req.url.replace('/','');
+    
+    if (isHTML) {
+        fs.readFile(fileName, (err, data) => {
+           res.setHeader('Content-Type','text/html');
+           if (err) res.end('<strong>Error html!</strong>');
+        res.end(String(data));
+        });
+        return;
+    }
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) res.end('<strong>Error not html!</strong>');
+        res.end(data);
+    });
+});
+
+
+server.listen(5600, () => {
+    console.log('Server is listening on port: 5600');
+});
