fe4d7b19cec71f008cea4b90668e40171761f2f8
Sergii <sergii@mabbly.com>
2018-11-11 18:16:28 +0200

    feat(server task_4): added display date

--- node-server/server.js
+++ node-server/server.js
@@ -1,16 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const prettyDate = require('./date');
 
 const server = http.createServer((req, res) => {
     const isHTML = req.url.includes('.html');
     const fileName = req.url.replace('/', '');
+    const styles = '<style>.pretty-date {position: fixed;bottom: 0;right: 0;}</style>';
+    const formatPrettyDate = '<div class="pretty-date">' + prettyDate() + '</div>';
 
     if (isHTML) {
         fs.readFile(fileName, (err, data) => {
             res.setHeader('Content-Type', 'text/html');
             if (err)
                 res.end('<strong>Error HTML!</strong>');
-            res.end(String(data));
+            res.end(styles + String(data) + formatPrettyDate);
         });
         return;
     }
@@ -21,7 +24,6 @@ const server = http.createServer((req, res) => {
         res.end(data);
     });
 });
-
 
 server.listen(5600, () => {
     console.log('Server is listening on port: 5600');
