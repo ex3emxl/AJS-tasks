ffd6e744be40dd2c14015ffd2247f9c545e84748
Sergii <sergii@mabbly.com>
2018-11-20 17:06:05 +0200

    feat(webpack-styles): tasks 1 - 3

---
+++ src/scripts/elementBuilder.js
@@ -0,0 +1,8 @@
+module.exports = 
+(tag = 'div', content = '', className = 'box') => {
+ const element = document.createElement(tag);
+ element.className = className;
+ element.innerHTML = content;
+
+ return element;
+};
