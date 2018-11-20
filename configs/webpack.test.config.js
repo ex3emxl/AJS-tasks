99c2c684758c015d0697798e72f88fca4ebbf494
Sergii <sergii@mabbly.com>
2018-11-20 21:38:43 +0200

    fix(tests)

---
+++ configs/webpack.test.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './app.js'
+    },
+    context: path.resolve(__dirname, '../src'),
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../public')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+    
+};
