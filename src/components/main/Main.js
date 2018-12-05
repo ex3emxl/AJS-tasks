9fcc0298781363b7a6b3cc676ce7cd50bb951d99
Sergii <sergii@mabbly.com>
2018-12-05 11:12:15 +0200

    react class components

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,13 +4,43 @@ import Content from '../content';
 
 import './main.scss';
 
+class Main extends Component{
+    state = { users: [] };
 
-const Main = () => (
-    <main className='main'>
-        <Sidebar />
+    constructor(props){
+        super(props);
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({users}))
+    }
 
-        <Content />
-    </main>
-);
+    showUserName(username) {
+        console.log( username);
+    }
+
+    render() {
+        const {title} = this.props;
+        const {users} = this.state;
+
+        return ( <main className='main'>
+            <h1>{title}</h1>
+            <Sidebar />
+            <ul>
+            {users.map(({name, id, username}) => <li key={id} onClick={() => this.showUserName(username)}>{name}</li>)}
+            </ul>
+            <Content />
+        </main>);
+    }
+}
+
+
+
+// const Main = () => (
+    {/*<main className='main'>*/}
+        {/*<Sidebar />*/}
+
+        {/*<Content />*/}
+    {/*</main>*/}
+// );
 
 export default Main;
